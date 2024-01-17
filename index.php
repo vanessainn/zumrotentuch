f44811aebd0177cc8cf7689292b637803aa5cd45
Vanessa Innerbichler <vanessa.innerbichler@gmail.com>
2024-01-17 14:46:54 +0100

    add fonts.

--- index.php
+++ index.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Zum Roten Tuch</title>
     <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/mein.css">
 </head>
 <body>
 
