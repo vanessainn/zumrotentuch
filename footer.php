7b762ce894d6647aae41ba5e82d95ac70e1a6eb8
Vanessa Innerbichler <vanessa.innerbichler@gmail.com>
2024-01-23 14:29:19 +0100

    add footer css.

--- footer.php
+++ footer.php
@@ -1,8 +1,8 @@
 <footer>
-    <div class="conainer-fluid">
+    <div class="conainer-fluid mt-5 pt-3 ps-3 pe-5">
         <div class="row">
             <div class="col-md-8">
-                <ul>
+                <ul class="d-flex gap-5">
                     <li><a href="impressum.php">Impressum</a></li>
                     <li><a href="datenschutz.php">Datenschutz</a></li>
                     <li><a href="images/agb.pdf" download>AGB</a></li>
@@ -11,7 +11,7 @@
             </div>
             <div class="col-md-4">
                 <ul class="list-unstyled d-flex justify-content-end gap-3">
-                    <li><a href="#"><i class="fab fa-facebook-f icon"></i></a></li>
+                    <li><a href="#"><i class="fab fa-facebook-f"></i></a></li>
                     <li><a href="#"><i class="fab fa-instagram"></i></a></li>
                 </ul>
             </div>
