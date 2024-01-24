07b90571ca25eac76e2d600772d5b5471206ed50
Vanessa Innerbichler <vanessa.innerbichler@gmail.com>
2024-01-24 14:18:20 +0100

    add timeline.

--- ueberuns.php
+++ ueberuns.php
@@ -15,7 +15,7 @@
                 <div class="row">
                     <div class="col-md-12">
                         <h1>Über Uns</h1>
-                        <img src="" alt="team">
+                        <img src="images/team.jpg" alt="team" class="img-fluid">
                         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi voluptatem similique tempore. Eum, ipsa exercitationem?</p>
                     </div>
                 </div>
@@ -33,36 +33,64 @@
             </div>
         </section>
 
-        <section id="schneiderei">
+        <section id="schneiderei" class="mb-5">
             <div class="container">
                 <div class="row">
                     <div class="col-md-12">
                         <h2>Unsere Schneiderei</h2>
                         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi voluptatem similique tempore. Eum, ipsa exercitationem?</p>
                     </div>
-                    <div class="col-md-6">
-                        <img src="" alt="schneiderei1">
+                    <div class="col-md-5">
+                        <img src="images/schere.jpg" alt="schneiderei1" class="img-fluid">
                     </div>
-                    <div class="col-md-6">
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis rem eligendi, alias ratione voluptatem doloremque quo impedit necessitatibus suscipit pariatur? A sed quisquam ea dolor ratione officia expedita, quae in molestiae tempora corrupti possimus distinctio obcaecati omnis cum. Quae, ex.</p>
+                    <div class="col-md-2 d-flex justify-content-center align-items-center">
+                        <div class="vr" style="height: 100%"></div>
+                        <div class="timeline"><img class="schritte" src="images/one.png" alt="Schritt Eins"></div>
                     </div>
-                    <div class="col-md-6">
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis rem eligendi, alias ratione voluptatem doloremque quo impedit necessitatibus suscipit pariatur? A sed quisquam ea dolor ratione officia expedita, quae in molestiae tempora corrupti possimus distinctio obcaecati omnis cum. Quae, ex.</p>
+                    <div class="col-md-5  d-flex align-items-center">
+                        <div class="my-5">
+                            <h3>Schritt 1</h3>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis rem eligendi, alias ratione voluptatem doloremque quo impedit necessitatibus suscipit pariatur? A sed quisquam ea dolor ratione officia expedita, quae in molestiae tempora corrupti possimus distinctio obcaecati omnis cum. Quae, ex.</p>
+                        </div>
                     </div>
-                    <div class="col-md-6">
-                        <img src="" alt="schneiderei2">
+                    <div class="col-md-5 d-flex align-items-center">
+                        <div class="mb-5">
+                            <h3>Schritt 2</h3>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis rem eligendi, alias ratione voluptatem doloremque quo impedit necessitatibus suscipit pariatur? A sed quisquam ea dolor ratione officia expedita, quae in molestiae tempora corrupti possimus distinctio obcaecati omnis cum. Quae, ex.</p>
+                        </div>
                     </div>
-                    <div class="col-md-6">
-                        <img src="" alt="schneiderei3">
+                    <div class="col-md-2 d-flex justify-content-center align-items-center">                            
+                        <div class="vr" style="height: 100%"></div>
+                        <div class="timeline"><img class="schritte" src="images/two.png" alt="Schritt Zwei"></div>
                     </div>
-                    <div class="col-md-6">
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis rem eligendi, alias ratione voluptatem doloremque quo impedit necessitatibus suscipit pariatur? A sed quisquam ea dolor ratione officia expedita, quae in molestiae tempora corrupti possimus distinctio obcaecati omnis cum. Quae, ex.</p>
+                    <div class="col-md-5">
+                        <img src="images/faden.jpg" alt="schneiderei2" class="img-fluid">
                     </div>
-                    <div class="col-md-6">
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis rem eligendi, alias ratione voluptatem doloremque quo impedit necessitatibus suscipit pariatur? A sed quisquam ea dolor ratione officia expedita, quae in molestiae tempora corrupti possimus distinctio obcaecati omnis cum. Quae, ex.</p>
+                    <div class="col-md-5">
+                        <img src="images/nähmaschine.jpg" alt="schneiderei3" class="img-fluid">
                     </div>
-                    <div class="col-md-6">
-                        <img src="" alt="schneiderei4">
+                    <div class="col-md-2 d-flex justify-content-center align-items-center">
+                        <div class="vr" style="height: 100%"></div>
+                        <div class="timeline"><img class="schritte" src="images/three.png" alt="Schritt Drei"></div>
+                    </div>
+                    <div class="col-md-5 d-flex align-items-center">
+                        <div class="my-5">
+                            <h3>Schritt 3</h3>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis rem eligendi, alias ratione voluptatem doloremque quo impedit necessitatibus suscipit pariatur? A sed quisquam ea dolor ratione officia expedita, quae in molestiae tempora corrupti possimus distinctio obcaecati omnis cum. Quae, ex.</p>
+                        </div>
+                    </div>
+                    <div class="col-md-5 d-flex align-items-center">
+                        <div class="mb-5">
+                            <h3>Schritt 4</h3>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis rem eligendi, alias ratione voluptatem doloremque quo impedit necessitatibus suscipit pariatur? A sed quisquam ea dolor ratione officia expedita, quae in molestiae tempora corrupti possimus distinctio obcaecati omnis cum. Quae, ex.</p>
+                        </div>
+                    </div>
+                    <div class="col-md-2 d-flex justify-content-center align-items-center">
+                        <div class="vr" style="height: 100%"></div>
+                        <div class="timeline"><img class="schritte" src="images/four.png" alt="Schritt Vier"></div>
+                    </div>
+                    <div class="col-md-5">
+                        <img src="images/Stoffe.jpg" alt="schneiderei4" class="img-fluid">
                     </div>
                 </div>
             </div>
