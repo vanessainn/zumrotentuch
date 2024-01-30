9f92cda056b741ee2f371004f96dace23886bf1d
Vanessa Innerbichler <vanessa.innerbichler@gmail.com>
2024-01-30 10:55:59 +0100

    update produkte.

--- produkte.php
+++ produkte.php
@@ -15,7 +15,7 @@
                 <div class="row">
                     <div class="col-md-12">
                         <h1>Unsere Produkte</h1>
-                        <img src="" alt="Produkte">
+                        <img src="images/produkte.jpg" alt="Produkte" class="img-fluid">
                         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum, obcaecati quod asperiores earum error iusto modi placeat alias aliquam, corporis minima ut! Cupiditate obcaecati eos voluptate, modi deleniti possimus cumque sed debitis praesentium explicabo blanditiis. Nobis doloribus necessitatibus fuga velit dolor repellat harum quam maxime iste. Autem unde magnam voluptatum in expedita, temporibus mollitia ea nemo, cum aliquam officiis a quibusdam nulla ab nisi, tenetur reiciendis totam saepe doloribus? Cum itaque a placeat quisquam aspernatur ab hic quod corrupti reprehenderit.</p>
                         <a href="/images/waschanleitung.pdf" download>Waschanleitung zum downloaden</a>
                     </div>
@@ -27,33 +27,23 @@
             <div class="container">
                 <div class="row">
                     <div class="col-md-6">
-                        <img src="" alt="Produkt1">
-                        <h4>Produkt 1</h4>
+                        <img src="images/schmuck.jpg" alt="Produkt1" class="img-fluid">
+                        <h4>Schmuck</h4>
                         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut corporis molestias illo sit non magnam accusantium reiciendis dolore temporibus nisi nam, dicta itaque enim quae perferendis. Autem dicta quo in!</p>
                     </div>
                     <div class="col-md-6">
-                        <img src="" alt="Produkt2">
-                        <h4>Produkt 2</h4>
+                    <img src="images/produkt1.jpg" alt="Produkt1" class="img-fluid">
+                        <h4>Gewandung Herren</h4>
                         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut corporis molestias illo sit non magnam accusantium reiciendis dolore temporibus nisi nam, dicta itaque enim quae perferendis. Autem dicta quo in!</p>
                     </div>
                     <div class="col-md-6">
-                        <img src="" alt="Produkt3">
-                        <h4>Produkt 3</h4>
+                    <img src="images/produkt1.jpg" alt="Produkt1" class="img-fluid">
+                        <h4>Gewandung Damen</h4>
                         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut corporis molestias illo sit non magnam accusantium reiciendis dolore temporibus nisi nam, dicta itaque enim quae perferendis. Autem dicta quo in!</p>
                     </div>
                     <div class="col-md-6">
-                        <img src="" alt="Produkt4">
-                        <h4>Produkt 4</h4>
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut corporis molestias illo sit non magnam accusantium reiciendis dolore temporibus nisi nam, dicta itaque enim quae perferendis. Autem dicta quo in!</p>
-                    </div>
-                    <div class="col-md-6">
-                        <img src="" alt="Produkt5">
-                        <h4>Produkt 5</h4>
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut corporis molestias illo sit non magnam accusantium reiciendis dolore temporibus nisi nam, dicta itaque enim quae perferendis. Autem dicta quo in!</p>
-                    </div>
-                    <div class="col-md-6">
-                        <img src="" alt="Produkt6">
-                        <h4>Produkt 6</h4>
+                    <img src="images/produkt1.jpg" alt="Produkt1" class="img-fluid">
+                        <h4>Gewandung Kinder</h4>
                         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut corporis molestias illo sit non magnam accusantium reiciendis dolore temporibus nisi nam, dicta itaque enim quae perferendis. Autem dicta quo in!</p>
                     </div>
                 </div>
