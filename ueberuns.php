a17a6492ddbc0cd482bdbc4946c360acb1423fe2
Vanessa Innerbichler <vanessa.innerbichler@gmail.com>
2024-02-01 20:40:10 +0100

    finalization ueberuns.

--- ueberuns.php
+++ ueberuns.php
@@ -15,59 +15,62 @@
                 <div class="row">
                     <div class="col-md-12">
                         <h1>Über Uns</h1>
-                        <img src="images/team.jpg" alt="team" class="img-fluid">
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi voluptatem similique tempore. Eum, ipsa exercitationem?</p>
+                        <img src="images/team.png" alt="team" class="img-fluid">
+                        <p class="mt-2 ms-4">Moni, Thomas und Oliver</p>
+                        <p>In unserem Familienunternehmen fließt Leidenschaft und Hingabe in jeden einzelnen Schritt. An der Spitze steht Moni, das Herz und der kreative Geist unserer Familie. Sie ist verantwortlich für die Entwürfe, die sorgfältige Auswahl der Stoffe und führt mit fundiertem historischem Wissen das Schneiderhandwerk aus. Thomas, der Erstgeborener, ist Experte im Zuschnitt der Stoffe und meistert die Konstruktion unserer Marktzelte und Stände sowie alle technischen Aspekte mit Bravour. Oliver, der Zweitgeborene, kümmert sich um sämtliche wirtschaftlichen und organisatorischen Angelegenheiten – von der Buchhaltung bis hin zur Planung unseres Marktterminkalenders.</p>
                     </div>
                 </div>
             </div>
         </section>
 
-        <section id="geschichte">
+        <section id="geschichte" class="mt-5">
             <div class="container">
                 <div class="row">
                     <div class="col-md-12">
                         <h2>Unsere Geschichte</h2>
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla delectus quod sed dolorem eius fugiat ratione doloribus officia obcaecati temporibus debitis eveniet nostrum consequatur fuga laboriosam fugit minima enim ipsum expedita adipisci sapiente dignissimos, illum numquam. Sint repudiandae incidunt quis ea ipsam. Soluta, facilis accusantium aperiam magnam sequi a fuga minima ducimus maxime pariatur tempora corporis voluptatibus eius perspiciatis sunt, officia asperiores natus, velit laudantium? Veritatis at, assumenda quas ut temporibus sapiente saepe autem recusandae? Labore aut maxime qui quisquam deserunt ex delectus tempora maiores fugiat, iusto veniam quod praesentium a, hic quidem magni porro! Quas harum saepe dolor in?</p>
+                        <p>Es begann mit der Neugierde. Monis unersättliches Verlangen, neue Horizonte zu erkunden, führte sie weg von der alltäglichen Routine des Schneiderns zeitgenössischer Kleidung hin zu neuen Abenteuern. Und wie das Leben es oft so will, fand sie diese in den traditionellen Materialien: Leinen, Wolle und Walkstoffen, kunstvoll zu historischer Gewandung verarbeitet.</p>
+                        <p>Nach intensivem Studium der historischen Schneiderei und dem Eintauchen in die faszinierende Welt der Mittelaltermärkte und -feste war der Entschluss gefasst und "Zum roten Tuch" geboren. Über die Jahre hinweg haben wir kontinuierlich an unseren Fertigkeiten gefeilt und unser Angebot erweitert, und so sind wir zu einem festen Bestandteil der deutschsprachigen Mittelalterszene geworden.</p>
+                        <p>"Zum roten Tuch" – unter diesem Wappen hüllen Sie sich in Gewänder längst vergessener Zeiten und legendären Kulturen.</p>
                     </div>
                 </div>
             </div>
         </section>
 
-        <section id="schneiderei" class="mb-5">
+        <section id="schneiderei" class="my-5">
             <div class="container">
                 <div class="row">
                     <div class="col-md-12">
                         <h2>Unsere Schneiderei</h2>
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi voluptatem similique tempore. Eum, ipsa exercitationem?</p>
+                        <p>Der Schneidereiprozess für Mittelalterkleidung umfasst mehrere Schritte, die darauf abzielen, historisch inspirierte Stücke mit Authentizität und Qualität herzustellen:</p>
                     </div>
                     <div class="col-md-5">
-                        <img src="images/schere.jpg" alt="schneiderei1" class="img-fluid">
+                        <img src="images/skizze.jpg" alt="notizbuch" class="img-fluid">
                     </div>
                     <div class="col-md-2 d-flex justify-content-center align-items-center">
                         <div class="vr" style="height: 100%"></div>
                         <div class="timeline"><img class="schritte" src="images/one.png" alt="Schritt Eins"></div>
                     </div>
-                    <div class="col-md-5  d-flex align-items-center">
+                    <div class="col-md-5 d-flex align-items-center">
                         <div class="my-5">
-                            <h3>Schritt 1</h3>
-                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis rem eligendi, alias ratione voluptatem doloremque quo impedit necessitatibus suscipit pariatur? A sed quisquam ea dolor ratione officia expedita, quae in molestiae tempora corrupti possimus distinctio obcaecati omnis cum. Quae, ex.</p>
+                            <h3>Entwurf und Mustererstellung</h3>
+                            <p>Entwürfe werden skizziert und Muster angefertigt. Dabei werden die Proportionen, Schnitte und Details der Kleidungsstücke festgelegt. Oft werden historische Schnittmuster als Ausgangspunkt verwendet und an moderne Passformen angepasst.</p>
                         </div>
                     </div>
                     <div class="col-md-5 d-flex align-items-center">
-                        <div class="mb-5">
-                            <h3>Schritt 2</h3>
-                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis rem eligendi, alias ratione voluptatem doloremque quo impedit necessitatibus suscipit pariatur? A sed quisquam ea dolor ratione officia expedita, quae in molestiae tempora corrupti possimus distinctio obcaecati omnis cum. Quae, ex.</p>
+                        <div class="my-5">
+                            <h3>Stoffauswahl und -vorbereitung</h3>
+                            <p>Nach der Festlegung der Entwürfe werden die geeigneten Stoffe ausgewählt, die der historischen Ära entsprechen. Dies können natürliche Materialien wie Leinen, Wolle oder Baumwolle sein. Die Stoffe werden dann gewaschen, gebügelt und zugeschnitten.</p>
                         </div>
                     </div>
-                    <div class="col-md-2 d-flex justify-content-center align-items-center">                            
+                    <div class="col-md-2 d-flex justify-content-center align-items-center">
                         <div class="vr" style="height: 100%"></div>
-                        <div class="timeline"><img class="schritte" src="images/two.png" alt="Schritt Zwei"></div>
+                        <div class="timeline"><img class="schritte" src="images/two.png" alt="Schritt Vier"></div>
                     </div>
                     <div class="col-md-5">
-                        <img src="images/faden.jpg" alt="schneiderei2" class="img-fluid">
+                        <img src="images/Stoffe.jpg" alt="verschiedene Stoffe" class="img-fluid">
                     </div>
                     <div class="col-md-5">
-                        <img src="images/nähmaschine.jpg" alt="schneiderei3" class="img-fluid">
+                        <img src="images/nähmaschine.jpg" alt="Nähmaschine" class="img-fluid">
                     </div>
                     <div class="col-md-2 d-flex justify-content-center align-items-center">
                         <div class="vr" style="height: 100%"></div>
@@ -75,14 +78,14 @@
                     </div>
                     <div class="col-md-5 d-flex align-items-center">
                         <div class="my-5">
-                            <h3>Schritt 3</h3>
-                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis rem eligendi, alias ratione voluptatem doloremque quo impedit necessitatibus suscipit pariatur? A sed quisquam ea dolor ratione officia expedita, quae in molestiae tempora corrupti possimus distinctio obcaecati omnis cum. Quae, ex.</p>
+                            <h3>Zuschnitt und Nähen</h3>
+                            <p>Anhand der Muster werden die Stoffe zugeschnitten. Anschließend werden die Teile mit Nadel und Faden oder einer Nähmaschine zusammengefügt. Traditionelle Nähtechniken, wie Handstiche oder Zierstiche, können verwendet werden, um den historischen Charakter zu betonen.</p>
                         </div>
                     </div>
                     <div class="col-md-5 d-flex align-items-center">
                         <div class="mb-5">
-                            <h3>Schritt 4</h3>
-                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis rem eligendi, alias ratione voluptatem doloremque quo impedit necessitatibus suscipit pariatur? A sed quisquam ea dolor ratione officia expedita, quae in molestiae tempora corrupti possimus distinctio obcaecati omnis cum. Quae, ex.</p>
+                            <h3>Anpassung und Details</h3>
+                            <p>Während des Nähens werden eventuelle Anpassungen vorgenommen, um sicherzustellen, dass das Kleidungsstück gut sitzt und die gewünschte Passform hat. Details wie Verzierungen, Stickereien, Borten oder Knöpfe werden hinzugefügt, um dem Kleidungsstück Authentizität und Individualität zu verleihen.</p>
                         </div>
                     </div>
                     <div class="col-md-2 d-flex justify-content-center align-items-center">
@@ -90,7 +93,23 @@
                         <div class="timeline"><img class="schritte" src="images/four.png" alt="Schritt Vier"></div>
                     </div>
                     <div class="col-md-5">
-                        <img src="images/Stoffe.jpg" alt="schneiderei4" class="img-fluid">
+                        <img src="images/schere.jpg" alt="Schere" class="img-fluid">
+                    </div>
+                    <div class="col-md-5">
+                        <img src="images/anprobe.jpg" alt="Umkleidekabine" class="img-fluid">
+                    </div>
+                    <div class="col-md-2 d-flex justify-content-center align-items-center">
+                        <div class="vr" style="height: 100%"></div>
+                        <div class="timeline"><img class="schritte_neu" src="images/five.png" alt="Schritt Drei"></div>
+                    </div>
+                    <div class="col-md-5 d-flex align-items-center">
+                        <div class="my-5">
+                            <h3>Anprobe und Abschluss</h3>
+                            <p>Nach Fertigstellung des Kleidungsstücks wird eine Anprobe durchgeführt, um sicherzustellen, dass es richtig sitzt und alle Details korrekt sind. Eventuelle letzte Anpassungen werden vorgenommen, bevor das Kleidungsstück fertiggestellt und für den Kunden bereit ist.</p>
+                        </div>
+                    </div>
+                    <div class="col-md-12 mt-4">
+                        <p>Diese Schritte erfordern Fachkenntnisse, handwerkliches Geschick und Liebe zum Detail, um hochwertige Mittelalterkleidung herzustellen, die den Charme und die Ästhetik vergangener Epochen einfängt.</p>
                     </div>
                 </div>
             </div>
