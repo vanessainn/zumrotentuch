09f7e97b7ba450f8b116a05373cc598d5b326ab8
Vanessa Innerbichler <vanessa.innerbichler@gmail.com>
2024-02-05 21:46:50 +0100

    update mobile map.

--- map_mobile.php
+++ map_mobile.php
@@ -48,7 +48,18 @@
         <p>+</p>
     </button>
     <div class="panel">
-    <p>Lorem ipsum...</p>
+        <div class="col-md-12 d-flex mt-3">
+            <i class="me-2"><img src="images/symbol.png" alt="Symbol" width="18px"></i></i>
+            <h5>Burgfest Kaprun</h5>
+        </div>
+        <div class="d-flex infos">
+            <div class="col-md-7">
+                <p>19. Juli 2024 - 21. Juli 2024</p>
+            </div>
+            <div class="col-md-5 text-end">
+                <p>Kaprun</p>
+            </div>
+        </div>
     </div>
 
     <button class="accordion d-flex justify-content-between">
@@ -56,7 +67,30 @@
         <p>+</p>
     </button>
     <div class="panel">
-    <p>Lorem ipsum...</p>
+        <div class="col-md-12 d-flex mt-3">
+            <i class="me-2"><img src="images/symbol.png" alt="Symbol" width="18px"></i></i>
+            <h5>Spectaculum zu Friesach</h5>
+        </div>
+        <div class="d-flex infos">
+            <div class="col-md-7">
+                <p>27. Juli 2024 - 28. Juli 2024</p>
+            </div>
+            <div class="col-md-5 text-end">
+                <p>Friesach</p>
+            </div>
+        </div>
+        <div class="col-md-12 d-flex mt-1">
+            <i class="me-2"><img src="images/symbol.png" alt="Symbol" width="18px"></i></i>
+            <h5>Ritterfest Burg Hochosterwitz</h5>
+        </div>
+        <div class="d-flex infos">
+            <div class="col-md-7">
+                <p>8. August 2024 - 11. August 2024</p>
+            </div>
+            <div class="col-md-5 text-end">
+                <p>Hochosterwitz</p>
+            </div>
+        </div>
     </div>
 
     <button class="accordion d-flex justify-content-between">
@@ -64,7 +98,102 @@
         <p>+</p>
     </button>
     <div class="panel">
-    <p>Lorem ipsum...</p>
+        <div class="col-md-12 d-flex mt-3">
+            <i class="me-2"><img src="images/symbol.png" alt="Symbol" width="18px"></i></i>
+            <h5>Mittelalter erleben</h5>
+        </div>
+        <div class="d-flex infos">
+            <div class="col-md-7">
+                <p>6. April 2024 - 7. April 2024</p>
+            </div>
+            <div class="col-md-5 text-end">
+                <p>Gloggnitz</p>
+            </div>
+        </div>
+        <div class="col-md-12 d-flex mt-1">
+            <i class="me-2"><img src="images/symbol.png" alt="Symbol" width="18px"></i></i>
+            <h5>Ritterfest Burgwiese Burg Lichtenstein</h5>
+        </div>
+        <div class="d-flex infos">
+            <div class="col-md-7">
+                <p>13. April 2024 - 14. April 2024</p>
+            </div>
+            <div class="col-md-5 text-end">
+                <p>Maria Enzersdorf</p>
+            </div>
+        </div>
+        <div class="col-md-12 d-flex mt-1">
+            <i class="me-2"><img src="images/symbol.png" alt="Symbol" width="18px"></i></i>
+            <h5>Lagerleben 2024</h5>
+        </div>
+        <div class="d-flex infos">
+            <div class="col-md-9">
+                <p>18. April 2024 - 21. April 2024</p>
+            </div>
+            <div class="col-md-3 text-end">
+                <p>Purgstall</p>
+            </div>
+        </div>
+        <div class="col-md-12 d-flex mt-1">
+            <i class="me-2"><img src="images/symbol.png" alt="Symbol" width="18px"></i></i>
+            <h5>Mittelalterfest in Klosterneuburg</h5>
+        </div>
+        <div class="d-flex infos">
+            <div class="col-md-7">
+                <p>4. Mai 2024 - 5. Mai 2024</p>
+            </div>
+            <div class="col-md-5 text-end">
+                <p>Klosterneuburg</p>
+            </div>
+        </div>
+        <div class="col-md-12 d-flex mt-1">
+            <i class="me-2"><img src="images/symbol.png" alt="Symbol" width="18px"></i></i>
+            <h5>Burgtreyben zu Plankenstein</h5>
+        </div>
+        <div class="d-flex infos">
+            <div class="col-md-9">
+                <p>9. Mai 2024 - 12. Mai 2024</p>
+            </div>
+            <div class="col-md-3 text-end">
+                <p>Plankenstein</p>
+            </div>
+        </div>
+        <div class="col-md-12 d-flex mt-1">
+            <i class="me-2"><img src="images/symbol.png" alt="Symbol" width="18px"></i></i>
+            <h5>Mittelalterliches Spektakel Hainburg</h5>
+        </div>
+        <div class="d-flex infos">
+            <div class="col-md-7">
+                <p>19. Mai 2024 - 20. Mai 2024</p>
+            </div>
+            <div class="col-md-5 text-end">
+                <p>Hainburg</p>
+            </div>
+        </div>
+        <div class="col-md-12 d-flex mt-1">
+            <i class="me-2"><img src="images/symbol.png" alt="Symbol" width="18px"></i></i>
+            <h5>Mittelalterfest Aggstein</h5>
+        </div>
+        <div class="d-flex infos">
+            <div class="col-md-9">
+                <p>15. Juni 2024 - 16. Juni 2024</p>
+            </div>
+            <div class="col-md-3 text-end">
+                <p>Aggstein</p>
+            </div>
+        </div>
+        <div class="col-md-12 d-flex mt-1">
+            <i class="me-2"><img src="images/symbol.png" alt="Symbol" width="18px"></i></i>
+            <h5>Gaudium zur Purg</h5>
+        </div>
+        <div class="d-flex infos">
+            <div class="col-md-7">
+                <p>30. August 2024 - 1. September 2024</p>
+            </div>
+            <div class="col-md-5 text-end">
+                <p>Purgstall</p>
+            </div>
+        </div>
     </div>
 
     <button class="accordion d-flex justify-content-between">
@@ -72,7 +201,30 @@
         <p>+</p>
     </button>
     <div class="panel">
-    <p>Lorem ipsum...</p>
+        <div class="col-md-12 d-flex mt-3">
+            <i class="me-2"><img src="images/symbol.png" alt="Symbol" width="18px"></i></i>
+            <h5>Weihnachtsmarkt</h5>
+        </div>
+        <div class="d-flex infos">
+            <div class="col-md-7">
+                <p>TBA</p>
+            </div>
+            <div class="col-md-5 text-end">
+                <p>Vöcklabruck</p>
+            </div>
+        </div>
+        <div class="col-md-12 d-flex mt-1">
+            <i class="me-2"><img src="images/symbol.png" alt="Symbol" width="18px"></i></i>
+            <h5>Weihnachtsmarkt</h5>
+        </div>
+        <div class="d-flex infos">
+            <div class="col-md-7">
+                <p>TBA</p>
+            </div>
+            <div class="col-md-5 text-end">
+                <p>Linz</p>
+            </div>
+        </div>
     </div>
 
     <button class="accordion d-flex justify-content-between">
@@ -80,7 +232,78 @@
         <p>+</p>
     </button>
     <div class="panel">
-    <p>Lorem ipsum...</p>
+        <div class="col-md-12 d-flex mt-3">
+            <i class="me-2"><img src="images/symbol.png" alt="Symbol" width="18px"></i></i>
+            <h5>Ritterfest Alt Erlaa</h5>
+        </div>
+        <div class="d-flex infos">
+            <div class="col-md-7">
+                <p>4. Mai 2024 - 5. Mai 2024</p>
+            </div>
+            <div class="col-md-5 text-end">
+                <p>Zirkuswiese</p>
+            </div>
+        </div>
+        <div class="col-md-12 d-flex mt-1">
+            <i class="me-2"><img src="images/symbol.png" alt="Symbol" width="18px"></i></i>
+            <h5>Montur und Pulverdampf</h5>
+        </div>
+        <div class="d-flex infos">
+            <div class="col-md-7">
+                <p>13. Juli 2024 - 14. Juli 2024</p>
+            </div>
+            <div class="col-md-5 text-end">
+                <p>Arsenal</p>
+            </div>
+        </div>
+        <div class="col-md-12 d-flex mt-1">
+            <i class="me-2"><img src="images/symbol.png" alt="Symbol" width="18px"></i></i>
+            <h5>Mittelalterfest Schloss Neugebäude</h5>
+        </div>
+        <div class="d-flex infos">
+            <div class="col-md-9">
+                <p>13. September 2024 - 15. September 2024</p>
+            </div>
+            <div class="col-md-3 text-end">
+                <p>Wien</p>
+            </div>
+        </div>
+        <div class="col-md-12 d-flex mt-1">
+            <i class="me-2"><img src="images/symbol.png" alt="Symbol" width="18px"></i></i>
+            <h5>Ritterturnier zu Pferde Laxenburg 2</h5>
+        </div>
+        <div class="d-flex infos">
+            <div class="col-md-7">
+                <p>21. September 2024 - 22. September 2024</p>
+            </div>
+            <div class="col-md-5 text-end">
+                <p>Laxenburg</p>
+            </div>
+        </div>
+        <div class="col-md-12 d-flex mt-1">
+            <i class="me-2"><img src="images/symbol.png" alt="Symbol" width="18px"></i></i>
+            <h5>Ritterturnier zu Pferde Laxenburg 3</h5>
+        </div>
+        <div class="d-flex infos">
+            <div class="col-md-9">
+                <p>28. September 2024 - 29. September 2024</p>
+            </div>
+            <div class="col-md-3 text-end">
+                <p>Laxenburg</p>
+            </div>
+        </div>
+        <div class="col-md-12 d-flex mt-1">
+            <i class="me-2"><img src="images/symbol.png" alt="Symbol" width="18px"></i></i>
+            <h5>HGM</h5>
+        </div>
+        <div class="d-flex infos">
+            <div class="col-md-7">
+                <p>TBA</p>
+            </div>
+            <div class="col-md-5 text-end">
+                <p>Arsenal</p>
+            </div>
+        </div>
     </div>
 
     <button class="accordion d-flex justify-content-between">
@@ -88,7 +311,30 @@
         <p>+</p>
     </button>
     <div class="panel">
-    <p>Lorem ipsum...</p>
+        <div class="col-md-12 d-flex mt-3">
+            <i class="me-2"><img src="images/symbol.png" alt="Symbol" width="18px"></i></i>
+            <h5>23. Ritterfest Burg Oberkapfenberg</h5>
+        </div>
+        <div class="d-flex infos">
+            <div class="col-md-7">
+                <p>22. Juni 2024 - 23. Juni 2024</p>
+            </div>
+            <div class="col-md-5 text-end">
+                <p>Oberkapfenberg</p>
+            </div>
+        </div>
+        <div class="col-md-12 d-flex mt-1">
+            <i class="me-2"><img src="images/symbol.png" alt="Symbol" width="18px"></i></i>
+            <h5>Zeitreise ins Mittelalter Eggenburg</h5>
+        </div>
+        <div class="d-flex infos">
+            <div class="col-md-7">
+                <p>7. September 2024 - 8. September 2024</p>
+            </div>
+            <div class="col-md-5 text-end">
+                <p>Eggenburg</p>
+            </div>
+        </div>
     </div>
 
     <button class="accordion d-flex justify-content-between">
@@ -96,7 +342,18 @@
         <p>+</p>
     </button>
     <div class="panel">
-    <p>Lorem ipsum...</p>
+        <div class="col-md-12 d-flex mt-3">
+            <i class="me-2"><img src="images/symbol.png" alt="Symbol" width="18px"></i></i>
+            <h5>Südtiroler Ritterspiele</h5>
+        </div>
+        <div class="d-flex infos">
+            <div class="col-md-7">
+                <p>16. August 2024 - 18. August 2024</p>
+            </div>
+            <div class="col-md-5 text-end">
+                <p>Schluderns</p>
+            </div>
+        </div>
     </div>
 
     <button class="accordion d-flex justify-content-between">
@@ -104,6 +361,101 @@
         <p>+</p>
     </button>
     <div class="panel">
-    <p>Lorem ipsum...</p>
+        <div class="col-md-12 d-flex mt-3">
+            <i class="me-2"><img src="images/symbol.png" alt="Symbol" width="18px"></i></i>
+            <h5>Mystica Hamelon</h5>
+        </div>
+        <div class="d-flex infos">
+            <div class="col-md-7">
+                <p>1. März 2024 - 3. April 2024</p>
+            </div>
+            <div class="col-md-5 text-end">
+                <p>Hameln</p>
+            </div>
+        </div>
+        <div class="col-md-12 d-flex mt-1">
+            <i class="me-2"><img src="images/symbol.png" alt="Symbol" width="18px"></i></i>
+            <h5>Oster- Ritterturnier Loreley</h5>
+        </div>
+        <div class="d-flex infos">
+            <div class="col-md-7">
+                <p>30. März 2024 - 31. März 2024</p>
+            </div>
+            <div class="col-md-5 text-end">
+                <p>St. Goarshausen</p>
+            </div>
+        </div>
+        <div class="col-md-12 d-flex mt-1">
+            <i class="me-2"><img src="images/symbol.png" alt="Symbol" width="18px"></i></i>
+            <h5>Ortenburger Ritterspiele</h5>
+        </div>
+        <div class="d-flex infos">
+            <div class="col-md-9">
+                <p>30. Mai 2024 - 2. Juni 2024</p>
+            </div>
+            <div class="col-md-3 text-end">
+                <p>Ortenburg</p>
+            </div>
+        </div>
+        <div class="col-md-12 d-flex mt-1">
+            <i class="me-2"><img src="images/symbol.png" alt="Symbol" width="18px"></i></i>
+            <h5>Mittelaltermarkt Stadt Laufen</h5>
+        </div>
+        <div class="d-flex infos">
+            <div class="col-md-7">
+                <p>28. Juni 2024 - 28. Juni 2024</p>
+            </div>
+            <div class="col-md-5 text-end">
+                <p>Laufen</p>
+            </div>
+        </div>
+        <div class="col-md-12 d-flex mt-1">
+            <i class="me-2"><img src="images/symbol.png" alt="Symbol" width="18px"></i></i>
+            <h5>Historisches Burgfest Burghausen</h5>
+        </div>
+        <div class="d-flex infos">
+            <div class="col-md-9">
+                <p>12. Juli 2024 - 14. Juli 2024</p>
+            </div>
+            <div class="col-md-3 text-end">
+                <p>Burghausen</p>
+            </div>
+        </div>
+        <div class="col-md-12 d-flex mt-1">
+            <i class="me-2"><img src="images/symbol.png" alt="Symbol" width="18px"></i></i>
+            <h5>Drachenfest</h5>
+        </div>
+        <div class="d-flex infos">
+            <div class="col-md-7">
+                <p>23. Juli 2024 - 28. Juli 2024</p>
+            </div>
+            <div class="col-md-5 text-end">
+                <p>Am Quast, Diemelstadt</p>
+            </div>
+        </div>
+        <div class="col-md-12 d-flex mt-1">
+            <i class="me-2"><img src="images/symbol.png" alt="Symbol" width="18px"></i></i>
+            <h5>Conquest of Mythodea</h5>
+        </div>
+        <div class="d-flex infos">
+            <div class="col-md-9">
+                <p>31. Juli 2024 - 4. August 2024</p>
+            </div>
+            <div class="col-md-3 text-end">
+                <p>Brokeloh</p>
+            </div>
+        </div>
+        <div class="col-md-12 d-flex mt-1">
+            <i class="me-2"><img src="images/symbol.png" alt="Symbol" width="18px"></i></i>
+            <h5>Cave Gladium</h5>
+        </div>
+        <div class="d-flex infos">
+            <div class="col-md-7">
+                <p>15. August 2024 - 18. August 2024</p>
+            </div>
+            <div class="col-md-5 text-end">
+                <p>Furth im Wald</p>
+            </div>
+        </div>
     </div>
 </div>
